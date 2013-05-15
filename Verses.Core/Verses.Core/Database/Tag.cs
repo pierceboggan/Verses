099b9965e0558492b07c3a49d9f16750a94ae4ed
Pierce <pierce@pierceboggan.com>
2013-05-14 19:30:29 -0500

    New "Compose" Dialog
    Added new compose dialog as well as some database updates

---
+++ Verses.Core/Verses.Core/Database/Tag.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Verses.Core
+{
+	public class Tag
+	{
+		public Tag()
+		{
+		}
+	}
+}
+
---
+++ Verses.iOS/Verses.iOS/Verses/VerseComposeDialog.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Verses.iOS
+{
+	public class VerseComposeDialog
+	{
+		public VerseComposeDialog()
+		{
+		}
+	}
+}
+
